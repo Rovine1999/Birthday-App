41f011fbd3ef1850dfb40b105024ec13e6fdd3e3
Rovine1999 <rovinewanjala99@gmail.com>
2020-10-04 12:54:20 +0300

    modified pages

--- js/script.js
+++ js/script.js
@@ -1,42 +1,41 @@
 function day (cc,yy,mm,dd){
-   return ((((cc/19)+1*cc-1)+((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7)+1
+    return ((((cc/19)+1*cc-1)+((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7)+1
   }
-  function  gd() {
-  var cc =parseInt(document.getElementById("Id1").value);
-  var yy =parseInt(document.getElementById("Id2").value);
-  var mm =parseInt(document.getElementById("Id3").value);
-  var dd =parseInt(document.getElementById("Id4").value);
-  var gender=(document.getElementById("Id5").value);
-  var result=day(cc,yy,mm,dd).toFixed();
-  var males = ["Kwasi", "kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-  var females = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-      if (gender == "male" && result == 0) {
-      alert(males[0]);
-    } else if (gender == "male" && result == 1) {
-      alert(males[1]);
-    } else if (gender == "male" && result == 2) {
-      alert(males[2]);
-     } else if (gender == "male" && result == 3) {
-      alert(males[3]);
-    } else if (gender == "male" && result == 4) {
-      alert(males[4]);
-    } else if (gender == "male" && result == 5) {
-      alert(males[5]);
-    } else if (gender == "male" && result == 6) {
-      alert(males[6]);
-    } else if (gender == "female" && result == 0) {
-      alert(females[0]);
-    } else if (gender == "female" && result == 1) {
-      alert(females[1]);
-    } else if (gender == "female" && result == 2) {
-      alert(females[2]);
-    } else if (gender == "female" && result == 3) {
-      alert(females[3]);
-    } else if (gender == "female" && result == 4) {
-      alert(females[4]);
-    } else if (gender == "female" && result == 5) {
-      alert(females[5]);
-    } else if (gender == "female" && result == 6) {
-      alert(females[6]);
+  function myClick(){
+    var year = document.getElementById("year").value;
+    var Month = document.getElementById("month").value;
+    var day = document.getElementById("day").value;
+    var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var rovine = new Date();
+    rovine.setDate(parseInt(day));
+    rovine.setMonth(parseInt(Month)-1);
+    rovine.setFullYear(parseInt(year));
+    var r = rovine.getDay();
+    if(r===0){
+        alert("Your born day is sunday");
+    }else if(r===1){
+        alert("Your born is monday");
+    }else if(r===2){
+        alert("Your born day is tuesday");
+    }else if(r===3){
+        alert("Your born day is wednesday");
+    }else if(r===4){
+        alert("Your born is thursday");
+    }else if(r===5){
+        alert("Your born is friday");
+    }else if(r===6){
+        alert("Your born day is saturday");
+    }
+    if(year==""||year>2056){
+        alert("insert a valid year");
+    }else if(Month=="" ||Month>12){
+        alert("Enter a valid month");
+    }else if(day=="" ||day>31){
+        alert("Enter a valid month");
+    }else if(document.getElementById("male").checked==true && document.getElementById("female").checked==false){
+        alert("Your Akan name is " + maleName[r]);
+    }else if(document.getElementById("male").checked==false && document.getElementById("female").checked==true){
+        alert("Your Akan name is " + femaleName[r]);
     }
 }
